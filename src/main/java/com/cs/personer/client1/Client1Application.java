4c0724db433d1759f73c6654429f6b2a0dedf6d6
Jack <1783461699@qq.com>
2019-08-03 16:05:47 +0800

    hystrix熔断器测试

--- src/main/java/com/cs/personer/client1/Client1Application.java
+++ src/main/java/com/cs/personer/client1/Client1Application.java
@@ -3,7 +3,9 @@ package com.cs.personer.client1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
+@EnableHystrix //开启熔断器
 @EnableDiscoveryClient
 @SpringBootApplication
 public class Client1Application {
--- src/main/java/com/cs/personer/client1/controller/TestController.java
+++ src/main/java/com/cs/personer/client1/controller/TestController.java
@@ -1,12 +1,18 @@
 package com.cs.personer.client1.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 熔断器默认设置
+ */
+@DefaultProperties(defaultFallback = "defaultFallback")
 @RestController
 @RequestMapping(value = "/test", produces = "application/json;charset=UTF-8")
 public class TestController {
@@ -19,7 +25,37 @@ public class TestController {
     }
 
     @GetMapping("/getFeign")
+    @HystrixCommand
     public String getFeign(@RequestParam String name) {
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return name + port;
+    }
+
+    /**
+     * 指定超时时间4s,覆盖默认配置.
+     */
+    @HystrixCommand(fallbackMethod = "fallback", commandProperties =
+            {@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",
+                    value = "4000")})
+    @GetMapping("/getHystrix")
+    public String hystrix(@RequestParam String name) {
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return name + port;
+    }
+
+    private String defaultFallback() {
+        return "默认提示:系统繁忙";
+    }
+
+    private String fallback(String name) {
+        return "系统繁忙";
     }
 }
